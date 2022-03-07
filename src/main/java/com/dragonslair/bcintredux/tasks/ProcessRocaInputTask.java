db57c2cbedf5f7a7188d8c3314a6a4b04bba62e2
Dylan-Cruz <dylan-cruz@live.com>
2022-03-06 20:28:01 -0500

    work on process roca input

--- src/main/java/com/dragonslair/bcintredux/tasks/ProcessRocaInputTask.java
+++ src/main/java/com/dragonslair/bcintredux/tasks/ProcessRocaInputTask.java
@@ -10,8 +10,12 @@ import software.amazon.awssdk.core.sync.ResponseTransformer;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.S3Object;
 
-import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Component
@@ -29,19 +33,45 @@ public class ProcessRocaInputTask {
     @Value("${aws.bucket.name}")
     private String bucketName;
 
+    // constants
+    private final static String regexToSplit = "(?!\\B\"[^\"]*),(?![^\"]*\"\\B)";
+    private final static String CONDITION_KEY = "Condition";
+    private final static String QUANTITY_KEY = "Add to Quantity";
+    private final static String SCRYFALL_ID_KEY = "Scryfall Id";
+    private final static String FOIL_KEY = "Foil";
+
     public void runTask() {
         for (S3Object s3o : getFilesToProcess()) {
             String key = s3o.key();
 
             // if the file has a valid extension
             if (validateFileExtension(key)) {
-                try {
-                    // get the file as a buffered stream
 
+                // we got the file open, lets process it
+                try (BufferedReader reader = getS3ObjectAsBufferedReader(key)) {
 
+                    // get the first line and run validations
+                    String headers = reader.readLine();
+                    Map<String, Integer> headersToIndexes = validateHeader(headers);
 
-                } catch (RuntimeException e) {
-                    rollbar.error(e, "An error occurred processing file for key " + key + ". We'll try again next time.");
+                    // headers are valid lets get our indexes
+                    final int scryfallIndex = headersToIndexes.get(SCRYFALL_ID_KEY);
+                    final int quantityIndex = headersToIndexes.get(QUANTITY_KEY);
+                    final int conditionIndex = headersToIndexes.get(CONDITION_KEY);
+                    final int foilIndex = headersToIndexes.get(FOIL_KEY);
+
+                    // read the lines into memory
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+
+                    }
+
+                    // process the lines
+                    
+
+                } catch (Exception e) {
+                    rollbar.error(e, "An error occurred processing file for key " + key + ".");
+                    continue;
                 }
             }
         }
@@ -71,28 +101,58 @@ public class ProcessRocaInputTask {
         return valid;
     }
 
-    private InputStream getS3ObjectAsInputStream(String key) {
-        return bcintS3Client.getObject(r -> r.bucket(bucketName).key(key), ResponseTransformer.toInputStream());
+    private BufferedReader getS3ObjectAsBufferedReader(String key) {
+            return new BufferedReader(
+                    new InputStreamReader(
+                            bcintS3Client.getObject(r -> r.bucket(bucketName).key(key), ResponseTransformer.toInputStream())
+                    )
+            );
     }
 
-    private boolean validateFileContents() {
+    /** Runs various checks on the file and header row throwing a runtime exception
+     * if an issue is present. Otherwise it returns a map of header field names
+     * to their respective indexes in the file
+     * @param headers
+     * @return Map of column names to their position
+     */
+    private Map<String, Integer> validateHeader(String headers) {
         // file is not empty
+        if (headers == null) {
+            throw new RuntimeException("The file is empty.");
+        }
+
         // fields are comma delimited
+        List<String> headerFields = Arrays.stream(headers.split(regexToSplit)).toList();
+        if (headerFields.size() <= 1) {
+            throw new RuntimeException("The header row isn't comma separated.");
+        }
+
+        // get a map of header names to index map
+        Map<String, Integer> headerToIndexMap = new HashMap<>();
+        for (int i = 0; i < headerFields.size(); i++) {
+            headerToIndexMap.put(headerFields.get(i), i);
+        }
+
         // column Scryfall Id is present
+        if (!headerToIndexMap.containsKey(SCRYFALL_ID_KEY)) {
+            throw new RuntimeException("Column " + SCRYFALL_ID_KEY + " is missing.");
+        }
+
         // column Add to Quantity is present
-        return false;
-    }
+        if (!headerToIndexMap.containsKey(QUANTITY_KEY)) {
+            throw new RuntimeException("Column " + QUANTITY_KEY + " is missing.");
+        }
 
-    private void processFile() {
-        // get the file
+        // column Condition is present
+        if (!headerToIndexMap.containsKey(CONDITION_KEY)) {
+            throw new RuntimeException("Column " + CONDITION_KEY + " is missing.");
+        }
 
-        // create an empty list of AddQuantityJobs
+        // column Foiling is present
+        if (!headerToIndexMap.containsKey(FOIL_KEY)) {
+            throw new RuntimeException("Column " + FOIL_KEY + " is missing.");
+        }
 
-        // for each row
-            // call mtg automation service to add the quantity
-
-        // write out the results to a file
-
-        // post the file to AWS
+        return headerToIndexMap;
     }
 }
